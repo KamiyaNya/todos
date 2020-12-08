967c22929bf5f86bc527df022f8c1ebcf2cd23c9
Kamiya_Nya <KamiyaNya@users.noreply.github.com>
2020-12-08 16:23:02 +0300

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+
+const start = async () => {
+    try {
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
