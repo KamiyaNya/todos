ea3e0a56e4d03cd637cf29e6224fb86157b92396
Kamiya_Nya <KamiyaNya@users.noreply.github.com>
2020-12-08 19:42:08 +0300

    todo-site

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const config = require('./config.json')
+const router = require('./routes/todosRoute')
+const path = require('path')
+
+const port = config.PORT
+
+const app = express()
+
+const hbs = exphbs.create({
+    extname: 'hbs'
+})
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(router)
 
 const start = async () => {
     try {
+        await mongoose.connect(config.dbURL, {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+
+        app.listen(port, () => {
+            console.log(`server start on port ${port}`)
+        })
 
     } catch (e) {
         console.log(e)
---
+++ routes/todosRoute.js
@@ -0,0 +1,37 @@
+const { Router } = require('express')
+const Todo = require('../model/Todo')
+const router = Router()
+
+router.get("/", async (req, res) => {
+    const todosss = await Todo.find({})
+    res.render('index', {
+        title: "Главная",
+        isIndex: true,
+        todosss
+    })
+})
+router.get("/create", (req, res) => {
+    res.render('create', {
+        title: "Добавить задачу",
+        isCreate: true,
+    })
+})
+router.post("/create", async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    })
+    console.log(todo)
+    await todo.save()
+    res.redirect('/')
+})
+
+router.post("/complete", async (req, res) => {
+    const todo = await Todo.findById(req.body.id)
+
+    todo.completed = true
+    await todo.save()
+    res.redirect('/')
+})
+
+
+module.exports = router
