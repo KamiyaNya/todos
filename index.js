6f319e6bc7b7d8a88d09d82b9b95cecfaa15267b
KamiyaNya <dikriss16@gmail.com>
2020-12-18 23:09:00 +0300

    m

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const router = require('./routes/todosRoute')
+const config = require('./config.json')
 const path = require('path')
 
 const port = process.env.PORT || 4000
-
+const dbURL = config.dbUrl
 const app = express()
 
 const hbs = exphbs.create({
@@ -15,15 +16,18 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(router)
 
 const start = async () => {
     try {
-        await mongoose.connect("", {
+        await mongoose.connect(dbURL, {
             useNewUrlParser: true,
-            useFindAndModify: false, useUnifiedTopology: true
+            useFindAndModify: false,
+            useUnifiedTopology: true
         })
 
         app.listen(port, () => {
