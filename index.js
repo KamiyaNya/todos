22773c8f25e1281db664ea4f68e1b3d6172e6cbe
KamiyaNya <dikriss16@gmail.com>
2020-12-18 22:58:27 +0300

    Revert "modify"
    This reverts commit 5bc4222ce3cc6c517b021d73ef84750c2c194d72.

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const router = require('./routes/todosRoute')
-const config = require('./config.json')
 const path = require('path')
 
 const port = process.env.PORT || 4000
-const dbURL = config.dbUrl
+
 const app = express()
 
 const hbs = exphbs.create({
@@ -16,18 +15,15 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(router)
 
 const start = async () => {
     try {
-        await mongoose.connect(dbURL, {
+        await mongoose.connect("", {
             useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
+            useFindAndModify: false, useUnifiedTopology: true
         })
 
         app.listen(port, () => {
